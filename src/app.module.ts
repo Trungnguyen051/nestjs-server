a1b7dffab65b886bbd80fa0097195975d8f6574d
Trung Nguyen <thetrungnguyen.work@gmail.com>
2023-11-13 11:25:01 +0700

    init user route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot(TypeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  async getAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<User> {
+    const user = this.userService.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    } else {
+      return user;
+    }
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Put(':id')
+  async put(@Param('id') id: number, @Body() user: User): Promise<User> {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    const user = this.userService.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    } else {
+      return this.userService.delete(id);
+    }
+  }
+}
